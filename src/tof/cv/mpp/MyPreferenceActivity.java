c1c9e5a66f0d60d9f8f4b2f56038facf1926a901
Christophe Versieux <christophe.versieux@gmail.com>
2012-01-18 18:18:17 +0100

    Strange behavior of the PreferenceFragment I had to fix. What a headhache!

--- src/tof/cv/mpp/MyPreferenceActivity.java
+++ src/tof/cv/mpp/MyPreferenceActivity.java
@@ -3,6 +3,7 @@ package tof.cv.mpp;
 import java.util.List;
 
 import tof.cv.mpp.Utils.Utils;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
@@ -32,133 +33,50 @@ public class MyPreferenceActivity extends SherlockPreferenceActivity implements
 	public void onCreate(Bundle savedInstanceState) {
 		Utils.setFullscreenIfNecessary(this);
 		super.onCreate(savedInstanceState);
-
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-		
+
 		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
 			addPreferencesFromResource(R.xml.activity_preferences);
 			addPreferencesFromResource(R.xml.activity_planner_preferences);
 			addPreferencesFromResource(R.xml.activity_twitter_preferences);
+			this.setContentView(R.layout.activity_preference);
 		}
+		
+		Preference pref = findPreference(getString(R.string.key_planner_da));
+		if (pref != null)
+			pref.setSummary(((ListPreference) pref).getEntry());
 
-		setContentView(R.layout.activity_preference);
+		pref = findPreference(getString(R.string.key_activity));
+		if (pref != null)
+			pref.setSummary(((ListPreference) pref).getEntry());
 
-		try {
-			Preference pref = findPreference("prefPseudo");
+		pref = findPreference("prefPseudo");
+		if (pref != null)
 			pref.setSummary(((EditTextPreference) pref).getText());
 
-			pref = findPreference(getString(R.string.key_activity));
-			pref.setSummary(((ListPreference) pref).getEntry());
-			
-			pref = findPreference(getString(R.string.key_planner_da));
-			pref.setSummary(((ListPreference) pref).getEntry());
-		} catch (Exception e) {
-		}
-
-		// setContentView(R.layout.fragment_tab_picker);
-		// 
-
-		// This way is buggy
-		/*
-		 * mPager = (ViewPager) findViewById(R.id.pager); mAdapter = new
-		 * MyPrefAdapter(getSupportFragmentManager(), this);
-		 * mPager.setAdapter(mAdapter);
-		 * 
-		 * TitlePageIndicator titleIndicator = (TitlePageIndicator)
-		 * findViewById(R.id.indicator); titleIndicator.setViewPager(mPager);
-		 * titleIndicator.setFooterIndicatorStyle(IndicatorStyle.Underline);
-		 * 
-		 * mPager.setCurrentItem(this.getIntent().getExtras().getInt("screen"));
-		 */
 	}
 
 	@Override
 	public void onBuildHeaders(List<Header> target) {
+		Log.i("","onBuildHeaders");
 		loadHeadersFromResource(R.xml.preference_headers, target);
+		this.setContentView(R.layout.activity_preference);
 	}
 
-	/*
-	 * @Override public boolean onOptionsItemSelected(MenuItem item) { switch
-	 * (item.getItemId()) { case (android.R.id.home): // app icon in ActionBar
-	 * is clicked; Go home Intent intent = new Intent(this,
-	 * PlannerActivity.class); intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-	 * startActivity(intent); return true; default: return
-	 * super.onOptionsItemSelected(item); } }
-	 * 
-	 * public class MyPrefAdapter extends FragmentPagerAdapter implements
-	 * TitleProvider {
-	 * 
-	 * PreferenceListFragment[] fragments; String[] titles;
-	 * 
-	 * public MyPrefAdapter(FragmentManager fm, Context context) { super(fm);
-	 * fragments = new PreferenceListFragment[3]; fragments[0] = new
-	 * PreferenceListFragment( R.xml.activity_preferences); fragments[1] = new
-	 * PreferenceListFragment( R.xml.activity_planner_preferences); fragments[2]
-	 * = new PreferenceListFragment( R.xml.activity_twitter_preferences);
-	 * 
-	 * titles = new String[] { "GENERAL", "PLANNER", "TWITTER" }; }
-	 * 
-	 * @Override public Fragment getItem(int position) { return
-	 * fragments[position]; }
-	 * 
-	 * @Override public int getCount() { return fragments.length; }
-	 * 
-	 * @Override public String getTitle(int position) { return titles[position];
-	 * }
-	 * 
-	 * }
-	 * 
-	 * public static class PrefFragment extends PreferenceFragment { static int
-	 * mNum;
-	 * 
-	 * static PrefFragment newInstance(int num) { PrefFragment f = new
-	 * PrefFragment(); // Supply num input as an argument. Bundle args = new
-	 * Bundle(); args.putInt("num", num); f.setArguments(args); return f; }
-	 * 
-	 * @Override public View onCreateView(LayoutInflater inflater, ViewGroup
-	 * container, Bundle savedInstanceState) { View v =
-	 * inflater.inflate(R.layout.fragment_station_list, container, false); mNum
-	 * = getArguments() != null ? getArguments().getInt("num") : 1; return v; }
-	 * }
-	 * 
-	 * List<PreferenceScreen> rootPref=new ArrayList<PreferenceScreen>() ;
-	 * 
-	 * @Override public void onPreferenceAttached(PreferenceScreen root, int
-	 * xmlId) { rootPref.add(root); Toast.makeText(getBaseContext(),
-	 * root.getSharedPreferences().toString(), Toast.LENGTH_LONG).show();
-	 * root.getSharedPreferences().registerOnSharedPreferenceChangeListener(
-	 * this); // General Prefs try { Preference pref =
-	 * root.findPreference("prefPseudo"); pref.setSummary(((EditTextPreference)
-	 * pref).getText());
-	 * 
-	 * pref = root.findPreference(getString(R.string.key_activity));
-	 * pref.setSummary(((ListPreference) pref).getEntry()); } catch (Exception
-	 * e) { }
-	 * 
-	 * // Planner Prefs try { Preference pref = root
-	 * .findPreference(getString(R.string.key_planner_da));
-	 * pref.setSummary(((ListPreference) pref).getEntry()); } catch (Exception
-	 * e) { }
-	 * 
-	 * // Twitter Prefs try { } catch (Exception e) { }
-	 * 
-	 * }
-	 */
+
 	@Override
 	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
 			String key) {
-		Log.i("", "X");
+		Log.i("", "PRE-HC");
 		if (key.contentEquals("prefPseudo")) {
 			Preference pref = findPreference("prefPseudo");
 			pref.setSummary(((EditTextPreference) pref).getText());
 		}
 		if (key.contentEquals(getString(R.string.key_activity))) {
-			Log.i("", "B");
 			Preference pref = findPreference(getString(R.string.key_activity));
 			pref.setSummary(((ListPreference) pref).getEntry());
 		}
 		if (key.contentEquals(getString(R.string.key_planner_da))) {
-			Log.i("", "C");
 			Preference pref = findPreference(getString(R.string.key_planner_da));
 			pref.setSummary(((ListPreference) pref).getEntry());
 		}
--- src/tof/cv/mpp/view/StockPreferenceFragment.java
+++ src/tof/cv/mpp/view/StockPreferenceFragment.java
@@ -10,25 +10,92 @@
 	
   From _The Busy Coder's Guide to Android Development_
     http://commonsware.com/Android
-*/
+ */
 
-   
 package tof.cv.mpp.view;
 
+import tof.cv.mpp.R;
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class StockPreferenceFragment extends PreferenceFragment {
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    
-    int res=getActivity()
-              .getResources()
-              .getIdentifier(getArguments().getString("resource"),
-                              "xml",
-                              getActivity().getPackageName());
-    
-    addPreferencesFromResource(res);
-  }
+public class StockPreferenceFragment extends PreferenceFragment implements
+		OnSharedPreferenceChangeListener {
+
+	Activity a;
+	SharedPreferences preferences;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		a = this.getActivity();
+		Log.i("", "Activity1 " + a);
+		preferences = PreferenceManager
+				.getDefaultSharedPreferences(getActivity());
+		preferences.registerOnSharedPreferenceChangeListener(this);
+
+		int res = getActivity().getResources().getIdentifier(
+				getArguments().getString("resource"), "xml",
+				getActivity().getPackageName());
+		addPreferencesFromResource(res);
+
+		Preference pref = findPreference(getString(R.string.key_planner_da));
+		if (pref != null)
+			pref.setSummary(((ListPreference) pref).getEntry());
+
+		pref = findPreference(getString(R.string.key_activity));
+		if (pref != null)
+			pref.setSummary(((ListPreference) pref).getEntry());
+
+		pref = findPreference("prefPseudo");
+		if (pref != null)
+			pref.setSummary(((EditTextPreference) pref).getText());
+
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		super.onCreateView(inflater, container, savedInstanceState);
+		return inflater.inflate(R.layout.activity_preference, null);
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+		Log.i("","PAUSE");
+		preferences.unregisterOnSharedPreferenceChangeListener(this);
+	}
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
+		Log.i("", "PREFChanged1 "+this.getActivity());
+		if (key.contentEquals("prefPseudo")) {
+			Log.i("", "PseudoChanged1");
+			Preference pref = findPreference("prefPseudo");
+			pref.setSummary(((EditTextPreference) pref).getText());
+		}
+		if (key.contentEquals(a.getString(R.string.key_activity))) {
+			Log.i("", "FirstChanged1");
+			Preference pref = findPreference(a.getString(R.string.key_activity));
+			pref.setSummary(((ListPreference) pref).getEntry());
+		}
+		if (key.contentEquals(a.getString(R.string.key_planner_da))) {
+			Preference pref = findPreference(a.getString(R.string.key_planner_da));
+			Log.i("", "PlannerChanged1"+pref);
+			pref.setSummary(((ListPreference) pref).getEntry());
+		}
+
+	}
 }
