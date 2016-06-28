10f76adb3af98544945e917a0c09b0ea7b5feb9e
QRSHIRT <christophe.versieux@gmail.com>
2016-06-28 11:39:39 +0200

    Detail added

--- BeTrains/src/main/java/tof/cv/mpp/NotifFragment.java
+++ BeTrains/src/main/java/tof/cv/mpp/NotifFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -23,11 +24,6 @@ import java.util.List;
  * interface.
  */
 public class NotifFragment extends Fragment {
-
-    // TODO: Customize parameter argument names
-    private static final String ARG_COLUMN_COUNT = "column-count";
-    // TODO: Customize parameters
-    private int mColumnCount = 1;
     private OnListFragmentInteractionListener mListener;
 
     /**
@@ -37,23 +33,10 @@ public class NotifFragment extends Fragment {
     public NotifFragment() {
     }
 
-    // TODO: Customize parameter initialization
-    @SuppressWarnings("unused")
-    public static NotifFragment newInstance(int columnCount) {
-        NotifFragment fragment = new NotifFragment();
-        Bundle args = new Bundle();
-        args.putInt(ARG_COLUMN_COUNT, columnCount);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        if (getArguments() != null) {
-            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
-        }
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setSubtitle(null);
     }
 
     @Override
@@ -61,20 +44,14 @@ public class NotifFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_notif_list, container, false);
 
-        // Set the adapter
         if (view instanceof RecyclerView) {
             Context context = view.getContext();
             RecyclerView recyclerView = (RecyclerView) view;
-            if (mColumnCount <= 1) {
-                recyclerView.setLayoutManager(new LinearLayoutManager(context));
-            } else {
-                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
-            }
+            recyclerView.setLayoutManager(new LinearLayoutManager(context));
             recyclerView.setAdapter(new MyNotifRecipeAdapter(DummyContent.ITEMS, mListener));
         }
         return view;
     }
-
 
     @Override
     public void onAttach(Context context) {
